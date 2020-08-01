b93d34fb9081c6a18154f037234cb29c61b8bee7
wyrover <wyrover@gmail.com>
2020-08-01 17:48:46 +0800

    * gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 const { series, parallel, src, dest } = require('gulp')
 
 const encryptZip = require('./encrypt-zip')
+const rename = require('gulp-rename')
 
 function files() {
   return src('./*.js')
     .pipe(encryptZip('encrypt'))
     .pipe(encryptZip('decrypt'))
+    .pipe(
+      rename((path) => {
+        return {
+          dirname: path.dirname,
+          basename: 'zhw_' + path.basename,
+          //extname: '.js',
+        }
+      })
+    )
     .pipe(dest('output'))
 }
 
